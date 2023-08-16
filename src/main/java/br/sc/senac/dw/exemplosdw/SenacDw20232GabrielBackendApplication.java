9aa87dadb2aa83cbe6ab82659774c122a920baa0
garciaa43 <gabrielcordeir.gg@gmail.com>
2023-08-16 09:55:03 -0300

    Primeiro Commit SpringBoot

---
+++ src/main/java/br/sc/senac/dw/exemplosdw/SenacDw20232GabrielBackendApplication.java
@@ -0,0 +1,13 @@
+package br.sc.senac.dw.exemplosdw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenacDw20232GabrielBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenacDw20232GabrielBackendApplication.class, args);
+	}
+
+}
