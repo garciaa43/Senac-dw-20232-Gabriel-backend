2de1377a188c1a8e56be00f950ef8bc7cc576332
garciaa43 <gabrielcordeir.gg@gmail.com>
2023-08-22 09:59:34 -0300

    Feito o CRUD inteiro.

---
+++ src/main/java/br/sc/senac/dw/exemplosdw/exception/CampoInvalidoException.java
@@ -0,0 +1,8 @@
+package br.sc.senac.dw.exemplosdw.exception;
+
+public class CampoInvalidoException extends Exception{
+
+   public CampoInvalidoException(String mensagem){
+       super(mensagem);
+   }
+}
